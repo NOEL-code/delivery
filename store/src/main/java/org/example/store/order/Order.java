e9a043475f2f3719cf7a40c0b0dabfe0d8645af9
NOEL-code <junusong12@naver.com>
2024-05-23 09:42:43 +0900

    feat: order entity update

--- store/src/main/java/org/example/store/order/Order.java
+++ store/src/main/java/org/example/store/order/Order.java
@@ -5,8 +5,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
+import org.example.store.member.Member;
 import org.example.store.product.Product;
 
 @Getter
@@ -16,11 +20,17 @@ public class Order {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "order_id")
-    Long id;
+    private Long id;
 
-    Product product; // Domain = Object
+    @OneToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product; // Domain = Object
 
-    int quantity;
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    private int quantity;
 
     public Order(Product orderdProduct, int quantity) {
         this.product = orderdProduct;
